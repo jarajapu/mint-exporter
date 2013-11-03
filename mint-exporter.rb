f5659be9725946c33477b5d1a1219fe0184646e7
jarajapu <jarajapu@gmail.com>
2013-11-03 14:59:26 -0800

    mod to support transaction type filter

--- mint-exporter.rb
+++ mint-exporter.rb
@@ -5,13 +5,14 @@ Bundler.require
 
 hostname = "https://wwws.mint.com/"
 
-unless ARGV.length == 2
-  puts "Usage: ruby #{$0} USERNAME PASSWORD"
+unless ARGV.length == 3
+  puts "Usage: ruby #{$0} USERNAME PASSWORD TRANSACTION_TYPE"
   exit 1
 end
 
 username = ARGV[0]
 password = ARGV[1]
+tx_type  = ARGV[2]
 
 agent = Mechanize.new
 agent.pluggable_parser.default = Mechanize::Download
@@ -23,4 +24,6 @@ form.username = username
 form.password = password
 form.submit
 
-puts agent.get(URI.join hostname, "/transactionDownload.event").body
+puts agent.get(URI.join hostname, "/transactionDownload.event").body.split(",")
+          .each_with_index.map{|elem,index|elem = elem.gsub(/[\n]/,'').gsub(/[\"]/,'')}
+          .each_slice(8).select{|line| line[5] == tx_type}.to_a
